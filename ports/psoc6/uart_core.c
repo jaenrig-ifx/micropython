1dfc009143d6daf3fdc9266d8ab17949d2518940
Jens Bargfrede <Jens.Bargfrede@infineon.com>
2022-10-24 17:49:40 +0200

    rework of Makefile and boards directory structure.

--- ports/psoc6/uart_core.c
+++ ports/psoc6/uart_core.c
@@ -31,12 +31,14 @@ int mp_hal_stdin_rx_chr(void) {
 
 // Send string of given length
 void mp_hal_stdout_tx_strn(const char *str, mp_uint_t len) {
+    int r = write(STDOUT_FILENO, str, len);
+    (void)r;
 
-    // Either convert str to \0 terminated string in order to be able to use printf
-    char buf[len + 1];
-    memcpy(buf, str, len);
-    buf[len] = 0;
-    printf(buf);
+    // // Either convert str to \0 terminated string in order to be able to use printf
+    // char buf[len + 1];
+    // memcpy(buf, str, len);
+    // buf[len] = 0;
+    // printf(buf);
 
     // or use underlying method directly => this is compiler dependent !
     // _write(STDOUT_FILENO, str, len);
