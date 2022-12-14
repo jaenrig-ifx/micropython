62de73b5d28889858946f669fee71a36369179b3
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2022-12-14 17:05:31 +0100

    ports/psoc6/utils/mpy-install.cmd: WIP install windows script.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

--- ports/psoc6/utils/mpy-install.cmd
+++ ports/psoc6/utils/mpy-install.cmd
@@ -1,34 +1,49 @@
 @echo off
+setlocal enabledelayedexpansion
 
-echo Welcome to the MicroPython PSoC6 installer!
+echo ################################################
+echo                 Welcome to the 
+echo  __  __ _            ___      _   _             
+echo ^|  \/  (_)__ _ _ ___^| _ \_  _^| ^|_^| ^|_  ___ _ _ 
+echo ^| ^|\/^| ^| / _^| '_/ _ \  _/ ^|^| ^|  _^| ' \/ _ \ ' \ 
+echo ^|_^|_ ^|_^|_\__^|_^| \___/_^|  \_, ^|\__^|_^|^|_\___/_^|^|_^|
+echo ^| _ \/ __^| ___ / __^|/ /  ^|__/                    
+echo ^|  _/\__ \/ _ \ (__/ _ \                         
+echo ^|_^|  ^|___/\___/\___\___/ 
+echo:
+echo                    Installer
+echo ################################################
 
 Rem Board selection
 set board=%1
-set board_list[0]=cy8cproto-062-4343w
+set board_list[0]=CY8CPROTO-062-4343W
+
 if [%board%]==[] (
     echo:
-    echo ###################################
-    echo Supported MicroPython PSoC6 boards: 
-    echo ###################################
-    echo:
-    echo 0 - CY8CPROTO-062-4343W (default^)
+    echo       Supported MicroPython PSoC6 boards
+    echo +---------+-----------------------------------+
+    echo ^|   ID    ^|              Board                ^|
+    echo +---------+-----------------------------------+
+    echo ^|   0     ^|  CY8CPROTO-062-4343W (default^)    ^|
+    echo +---------+-----------------------------------+
     echo:
     echo No user selection required. Only one choice.
-	set board_index=0
+	set /a board_index=0
 	echo:
-    Rem set  /p/( "board_index=Please type the desired board number. " --> Uncomment and remove preselection above when more options are available
+    Rem set  /p/( "board_index=Please type the desired board ID. " --> Uncomment and remove preselection above when more options are available
+    call set board=%%board_list[!board_index!]%%
 )
-call echo MicroPython PSoC Board   :: %%board_list[%board_index%]%%
+echo MicroPython PSoC6 Board  :: %board%
 
 Rem Version selection
 set mpy_ifx_version=%2
 if [%mpy_ifx_version%]==[] set mpy_ifx_version=latest
 
-echo Micropython PSoC Version :: %mpy_ifx_version% 
+echo MicroPython PSoC Version :: %mpy_ifx_version% 
 echo:
 
 echo Downloading MicroPython PSoC6 port %mpy_ifx_version% for %board% board...
-Rem curl.exe -s -L https://github.com/jaenrig-ifx/micropython/releases/download/%mpy_ifx_version%/micropython-ifx-%board%.hex > micropython-ifx-%board%.hex
+Rem curl.exe -s -L https://github.com/jaenrig-ifx/micropython/releases/download/%mpy_ifx_version%/micropython-ifx-!board!.hex > micropython-ifx-!board!.hex
 
 echo Downloading openocd...
 curl.exe -s -L https://github.com/Infineon/openocd/releases/download/release-v4.4.0/openocd-4.4.0.2134-windows.zip > openocd.zip
@@ -49,15 +64,12 @@ Rem del micropython-ifx-%board%.hex
 del openocd.zip 
 rmdir /s /q openocd
 
-echo  __  __ _            ___      _   _             
-echo ^|  \/  (_)__ _ _ ___^| _ \_  _^| ^|_^| ^|_  ___ _ _ 
-echo ^| ^|\/^| ^| / _^| '_/ _ \  _/ ^|^| ^|  _^| ' \/ _ \ ' \ 
-echo ^|_^|_ ^|_^|_\__^|_^| \___/_^|  \_, ^|\__^|_^|^|_\___/_^|^|_^|
-echo ^| _ \/ __^| ___ / __^|/ /  ^|__/                    
-echo ^|  _/\__ \/ _ \ (__/ _ \                         
-echo ^|_^|  ^|___/\___/\___\___/  
-echo:      
-echo installation is ready. Enjoy!                 
-                                                  
+
+echo:   
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo  The installation is completed. Enjoy :)                 
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                                              
+
 echo:
-pause	
+echo Press any quit to exit...
+pause >nul
