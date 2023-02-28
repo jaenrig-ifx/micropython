702fa7314fc90b841c15672ea2418ad33eda5255
Jens Bargfrede <Jens.Bargfrede@infineon.com>
2023-02-28 09:50:26 +0100

    testing: Finalize testing and full cleanup of mpconfigport.h.

--- ports/psoc6/mpconfigport.h
+++ ports/psoc6/mpconfigport.h
@@ -3,7 +3,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2020-2021 Damien P. George
+ * Copyright (c) 2022-2023 Infineon Technologies AG
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -26,7 +26,6 @@
 
 // Options controlling how MicroPython is built, overriding defaults in py/mpconfig.h
 #include <stdint.h>
-#include "mpconfigboard.h"
 
 // Board and hardware specific configuration
 #define MICROPY_HW_MCU_NAME                     "PSoC62"
@@ -34,13 +33,16 @@
 #define MICROPY_HW_ENABLE_UART_REPL             (0) // useful if there is no USB
 #define MICROPY_HW_ENABLE_USBDEV                (0)
 
-#ifndef MICROPY_CONFIG_ROM_LEVEL
 #define MICROPY_CONFIG_ROM_LEVEL                (MICROPY_CONFIG_ROM_LEVEL_FULL_FEATURES)
-#endif
 
 // Memory allocation policies
 #define MICROPY_GC_STACK_ENTRY_TYPE             uint16_t
-#define MICROPY_ALLOC_PATH_MAX                  (256)
+#define MICROPY_ENABLE_GC                       (1)
+#define MICROPY_GC_HEAP_SIZE                    (256 * 1024)
+
+#define MICROPY_MEM_STATS                       (1)
+#define MICROPY_MALLOC_USES_ALLOCATED_SIZE      (1)
+#define MICROPY_TRACKED_ALLOC                   (MICROPY_SSL_MBEDTLS)
 #define MICROPY_QSTR_BYTES_IN_HASH              (1)
 
 // MicroPython emitters
@@ -55,8 +57,6 @@
 #define MICROPY_OPT_COMPUTED_GOTO               (0)
 
 // Python internal features
-#define MICROPY_READER_VFS                      (1)
-#define MICROPY_ENABLE_GC                       (1)
 #define MICROPY_ENABLE_EMERGENCY_EXCEPTION_BUF  (1)
 
 #define MICROPY_LONGINT_IMPL                    (MICROPY_LONGINT_IMPL_MPZ)
@@ -66,18 +66,16 @@
 #define MICROPY_SCHEDULER_DEPTH                 (8)
 #define MICROPY_SCHEDULER_STATIC_NODES          (1)
 
-#ifndef MICROPY_USE_INTERNAL_ERRNO
-#define MICROPY_USE_INTERNAL_ERRNO              (0)
-#endif
-
 // Fine control over Python builtins, classes, modules, etc
-#define MICROPY_PY_BUILTINS_HELP_TEXT           psoc6_help_text
 #define MICROPY_PY_SYS_PLATFORM                 "psoc6"
+#define MICROPY_PY_SYS_EXC_INFO                 (1)
+
 #define MICROPY_PY_THREAD                       (0)
-#define MICROPY_PY_THREAD_GIL                   (0)
+// #define MICROPY_PY_THREAD_GIL                   (0)
 
 // Extended modules
 #define MICROPY_EPOCH_IS_1970                   (1)
+
 #define MICROPY_PY_UOS_INCLUDEFILE              "ports/psoc6/modules/os/moduos.c"
 #define MICROPY_PY_UOS_UNAME                    (1)
 #define MICROPY_PY_UOS_URANDOM                  (0)
@@ -86,139 +84,66 @@
 
 #define MICROPY_PY_URE_MATCH_GROUPS             (1)
 #define MICROPY_PY_URE_MATCH_SPAN_START_END     (1)
+#define MICROPY_PY_URE_DEBUG                    (1)
+
 #define MICROPY_PY_UTIME_MP_HAL                 (1)
+
 #define MICROPY_PY_MACHINE                      (1)
 #define MICROPY_PY_MACHINE_PIN_MAKE_NEW         mp_pin_make_new
-#define MICROPY_PY_MACHINE_BITSTREAM            (0)
-#define MICROPY_PY_MACHINE_PULSE                (0)
 #define MICROPY_PY_MACHINE_PWM                  (0)
 #define MICROPY_PY_MACHINE_PWM_DUTY_U16_NS      (0)
 #define MICROPY_PY_MACHINE_I2C                  (1)
 #define MICROPY_PY_MACHINE_SOFTI2C              (1)
-#define MICROPY_PY_MACHINE_I2C_TRANSFER_WRITE1  (0)
 
-#define MICROPY_PY_MACHINE_SPI                  (0)
-#define MICROPY_PY_MACHINE_SOFTSPI              (0)
-#define MICROPY_PY_ONEWIRE                      (0)
+// VFS
 #define MICROPY_VFS                             (1)
-
-#define MICROPY_VFS_FAT                         (0)
+#define MICROPY_READER_VFS                      (1)
 
 // fatfs configuration
 #define MICROPY_FATFS_ENABLE_LFN                (1)
 #define MICROPY_FATFS_LFN_CODE_PAGE             437 /* 1=SFN/ANSI 437=LFN/U.S.(OEM) */
 #define MICROPY_FATFS_RPATH                     (2)
 
-// set to 1 to enable filesystem to be loaded on external qspi flash
-// if set to 0, filesystem is located in an allotted area of internal flash of PSoC6
-#define MICROPY_ENABLE_EXT_QSPI_FLASH               (1)
-
-#ifndef MICROPY_BOARD_ENTER_BOOTLOADER
-#define MICROPY_BOARD_ENTER_BOOTLOADER(nargs, args)
-#endif
-
 // By default networking should include sockets, ssl, websockets, webrepl, dupterm.
-// #define MICROPY_PY_NETWORK              (1)
+// #define MICROPY_PY_NETWORK                      (1)
+// #define MICROPY_PY_USOCKET                      (1)
+// #define MICROPY_PY_UWEBSOCKET                   (1)
+// #define MICROPY_PY_WEBREPL                      (1)
+#define MICROPY_PY_OS_DUPTERM                      (1)
 
-#if MICROPY_PY_NETWORK
+#define MP_STATE_PORT                           MP_STATE_VM
 
-#ifndef MICROPY_PY_USOCKET
-#define MICROPY_PY_USOCKET              (1)
-#endif
+#define MICROPY_PY_UCRYPTOLIB                   (1)
+#define MICROPY_PY_UCRYPTOLIB_CTR               (1)
+#define MICROPY_PY_UCRYPTOLIB_CONSTS            (1)
 
-#ifndef MICROPY_PY_USSL
-#define MICROPY_PY_USSL                 (1)
-#endif
-
-#ifndef MICROPY_PY_UWEBSOCKET
-#define MICROPY_PY_UWEBSOCKET           (1)
-#endif
-
-#ifndef MICROPY_PY_UHASHLIB_SHA1
-#define MICROPY_PY_UHASHLIB_SHA1        (1)
-#endif
-
-#ifndef MICROPY_PY_WEBREPL
-#define MICROPY_PY_WEBREPL              (1)
-#endif
-
-#ifndef MICROPY_PY_OS_DUPTERM
-#define MICROPY_PY_OS_DUPTERM           (1)
-#endif
-
-#endif
-
-
-#define MICROPY_TRACKED_ALLOC        (MICROPY_SSL_MBEDTLS)
-
-#define MICROPY_PY_UCRYPTOLIB        (1)
-#define MICROPY_PY_UCRYPTOLIB_CTR    (1)
-#define MICROPY_PY_UCRYPTOLIB_CONSTS (1)
-
-#define MICROPY_PY_UHASHLIB          (1)
-#define MICROPY_PY_UHASHLIB_MD5      (1)
-#define MICROPY_PY_UHASHLIB_SHA1     (1)
-#define MICROPY_PY_UHASHLIB_SHA256   (1)
-
-#define MP_STATE_PORT MP_STATE_VM
+#define MICROPY_PY_UHASHLIB_MD5                 (1)
+#define MICROPY_PY_UHASHLIB_SHA1                (1)
 
 // Miscellaneous settings
-#define MICROPY_MAKE_POINTER_CALLABLE(p) ((void *)((mp_uint_t)(p) | 1))
+#define MICROPY_MAKE_POINTER_CALLABLE(p)        ((void *)((mp_uint_t)(p) | 1))
 
 #define MP_SSIZE_MAX (0x7fffffff)
-typedef intptr_t mp_int_t; // must be pointer size
+typedef intptr_t mp_int_t;  // must be pointer size
 typedef uintptr_t mp_uint_t; // must be pointer size
 typedef intptr_t mp_off_t;
 
 // We need to provide a declaration/definition of alloca()
 #include <alloca.h>
 
-
-////////////////////////////////////////////////////////////////////////
-// TODO: Remove options implicitly set by setting feature level. Must work for all feature levels !
-// the following entries have been added by JB
-// Usually passed from Makefile
-#ifndef MICROPY_GC_HEAP_SIZE
-#define MICROPY_GC_HEAP_SIZE (256 * 1024)
-#endif
+#define MICROPY_PY_BUILTINS_HELP_TEXT           psoc6_help_text
 
 
-#define MICROPY_LOGGER_DEBUG              (0)
+// set in Makefile
+// #define MICROPY_QSTR_EXTRA_POOL                 mp_qstr_frozen_const_pool
 
+#define MICROPY_USE_INTERNAL_PRINTF             (0)
 
-#define MICROPY_PY_BUILTINS_HELP          (1)
-#define MICROPY_PY_BUILTINS_HELP_MODULES  (1)
-
-
-
-#define MICROPY_ENABLE_COMPILER           (1)
-#define MICROPY_QSTR_EXTRA_POOL           mp_qstr_frozen_const_pool
-#define MICROPY_ALLOC_PARSE_CHUNK_INIT    (16)
-#define MICROPY_KBD_EXCEPTION             (1)
-
-#define MICROPY_ENABLE_SOURCE_LINE        (1)
-
-#define MICROPY_USE_INTERNAL_PRINTF       (0)
-#define MICROPY_ENABLE_FINALISER          (1)
-#define MICROPY_VFS_POSIX                 (0)
-#define MICROPY_READER_POSIX              (0)
-
-
-#define MICROPY_REPL_INFO                 (1)
+#define MICROPY_REPL_INFO                       (1)
 
 // Set to zero explicitly ! Or mounting the /flash fs must be done inside try/except for both branches !
 // #define MICROPY_MODULE_BUILTIN_INIT    (0)
 
-#define MICROPY_PY_MICROPYTHON            (1)
-#define MICROPY_PY_GC                     (1)
-#define MICROPY_PY_IO                     (1)
-#define MICROPY_PY_IO_IOBASE              (1)
-
-
-#define MICROPY_PY_USELECT                (1)
-#define MICROPY_PY_USELECT_SELECT         (1)
-
-#define MICROPY_STACK_CHECK               (1)
 
 // TODO: helpful to abstract main.c ?
 // #define MICROPY_PORT_INIT_FUNC ??
@@ -227,14 +152,16 @@ typedef intptr_t mp_off_t;
 
 #include "shared/runtime/interrupt_char.h"
 
+// Potentially to be changed. Impact / improvements unclear !
+// #define MICROPY_OBJ_REPR                        (MICROPY_OBJ_REPR_A)
 
-#define MICROPY_OBJ_REPR (MICROPY_OBJ_REPR_A)
 
 #define MICROPY_EVENT_POLL_HOOK_FAST \
     do { \
         extern void mp_handle_pending(bool); \
         mp_handle_pending(true); \
     } while (0)
+
 
 #define MICROPY_EVENT_POLL_HOOK \
     do { \
@@ -244,12 +171,13 @@ typedef intptr_t mp_off_t;
 // best_effort_wfe_or_timeout(make_timeout_time_ms(1));
 
 
+// PSoC6 port specific
+// set to 1 to enable filesystem to be loaded on external qspi flash
+// if set to 0, filesystem is located in an allotted area of internal flash of PSoC6
+#define MICROPY_ENABLE_EXT_QSPI_FLASH           (1)
+#define MICROPY_LOGGER_DEBUG                    (0)
 
-#define MICROPY_PY_SYS_EXC_INFO      (1)
 
-#define MICROPY_PY_URE_DEBUG                (1)
-#define MICROPY_PY_URE_MATCH_GROUPS         (1)
-#define MICROPY_PY_URE_MATCH_SPAN_START_END (1)
-
-#define MICROPY_MEM_STATS (1)
-#define MICROPY_MALLOC_USES_ALLOCATED_SIZE (1)
+// Board specific settings.
+// DO NOT ADD ANY POT SPECIFC OPTIONS BELOW THE FOLLOWING LINE !
+#include "mpconfigboard.h"
