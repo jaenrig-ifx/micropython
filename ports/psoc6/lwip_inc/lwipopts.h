5ee547af14e2a01a8732be876cc6ca68b85ec67e
Jens Bargfrede <Jens.Bargfrede@infineon.com>
2023-04-04 16:42:01 +0200

    lwIP: Fixed inconsistency of settings used in MTB and MPY.

--- ports/psoc6/lwip_inc/lwipopts.h
+++ ports/psoc6/lwip_inc/lwipopts.h
@@ -1,11 +1,11 @@
 
-// Including file from MTB assets to make sure they match. 
+// Including file from MTB assets to make sure they match.
 // Workaround : in that file LWIP_IPV6 must be set to zero due to an MPY bug. This is achieved by a perl patch
 // in the Makefile, target "all". To be improved.
 #include "boards/mtb_shared/wifi-core-freertos-lwip-mbedtls/release-v1.0.0/configs/lwipopts.h"
 
 // changed by JB because of modlwip issue if both IP4 and IP6 are enabled !
 // #undef  LWIP_IPV6
-// #define LWIP_IPV6                       (0)
+// #define LWIP_IPV6                      (0)
 
 typedef uint32_t sys_prot_t;
